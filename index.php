4dfe94ebe812040be14b3fbe2585fbd7b17eb385
danny205 <5roundsdanny@gmail.com>
2016-10-03 14:51:31 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1313823
+ * Date: 03/10/2016
+ * Time: 14:41
+ */
+
+$age = 21;
+?>
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+
+<p>
+
+    <?php
+
+    if($age >= 16 AND $age <= 18){
+
+        echo "You can buy specs";
+
+    }elseif($age >= 18 AND $age <= 21){
+
+        echo "You can buy specs and mugs";
+
+    }elseif($age >= 21){
+
+        echo "You can buy specs, mugs and sausage rolls";
+
+    }
+
+    ?>
+
+</p>
+
+</body>
+</html>
