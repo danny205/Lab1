dae190a978e4c57d3a64987c4ee55b2f4338e876
danny205 <5roundsdanny@gmail.com>
2016-10-10 14:15:14 +0100

    ud1

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
-<?php
-
-?>
 <!DOCTYPE html>
 <html>
 <head></head>
 <body>
 
 <p>
-gergte
+    <?php
+
+    $con = mysqli_connect("us-cdbr-azure-southcentral-f.cloudapp.net","b42d75f25c6292","43152099","dannydb");
+
+    // Check connection
+    if (mysqli_connect_errno()){
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+    
+    ?>
 </p>
 
 </body>
