47838f418849e804f121ba744ed91b0de9021af3
danny205 <5roundsdanny@gmail.com>
2016-10-10 14:11:33 +0100

    ud1

--- index.php
+++ index.php
@@ -7,7 +7,17 @@
 <body>
 
 <p>
-rtueryueru
+    <?php
+
+    $db = new mysqli("us-cdbr-azure-southcentral-f.cloudapp.net","b42d75f25c6292","43152099","dannydb");
+
+    if($db->connect_errno){
+        die(Connectfailed[".$db->connect_error."]);
+    }else{
+        echo 'working';
+    }
+
+    ?>
 </p>
 
 </body>
