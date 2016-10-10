e6d03c14b994cb3437538d6e0ad2b919574aa1a3
danny205 <5roundsdanny@gmail.com>
2016-10-10 14:44:47 +0100

    ud1

--- index.php
+++ index.php
@@ -8,14 +8,13 @@
 
     $con = mysqli_connect("us-cdbr-azure-southcentral-f.cloudapp.net","b42d75f25c6292","43152099","dannydb");
 
-    // Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }else{
         echo "working";
     }
 
-    $sql = "SELECT title FROM marvelmovies  WHERE yearReleased='2003'";
+    $sql = "SELECT title FROM marvelmovies";
     $result = $con->query($sql);
 
     if ($result->num_rows > 0) {
