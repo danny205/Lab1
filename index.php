f683f5ba50b299510b4c2d33020c8deebb4e3c74
danny205 <5roundsdanny@gmail.com>
2016-10-10 14:16:04 +0100

    ud1

--- index.php
+++ index.php
@@ -11,8 +11,10 @@
     // Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }else{
+        echo "working";
     }
-    
+
     ?>
 </p>
 
