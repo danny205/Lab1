dcbb54f81602a1771b1cd1528410e4969af1e946
danny205 <5roundsdanny@gmail.com>
2016-10-17 13:25:51 +0100

    ud1

--- dbconnect.php
+++ dbconnect.php
@@ -2,8 +2,7 @@
 
 $con = mysqli_connect("us-cdbr-azure-southcentral-f.cloudapp.net","b42d75f25c6292","43152099","dannydb");
 
-if (mysqli_connect_errno()){
+if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
-$con->close();
 ?>
