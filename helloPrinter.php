b50e1b963c1274d2202494e6f734cbf31eb24cdd
danny205 <5roundsdanny@gmail.com>
2016-10-17 13:34:13 +0100

    ud1

---
+++ helloPrinter.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1313823
+ * Date: 17/10/2016
+ * Time: 13:29
+ */
+
+$plannet = $GET_[“plannet”];
+echo "Hello ".$plannet.".";
+?>
---
+++ helloSelector.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1313823
+ * Date: 17/10/2016
+ * Time: 13:28
+ */
+?>
+<a href="http://lab1ds.azurewebsites.net/helloPrinter.php?plannet=Earth">Earth</a>
+<a href="http://lab1ds.azurewebsites.net/helloPrinter.php?plannet=Mars">Mars</a>
+<a href="http://lab1ds.azurewebsites.net/helloPrinter.php?plannet=Uranus">Uranus</a>
+
+
